02c45af7b70b35ead90c3b51b8b8fddda5af8ec1
cheahshuen <kche0071@student.monash.edu>
2021-03-09 12:37:47 +0800

    Modified code and able to navigate to database, upload screen.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,37 @@
-import React, { Component } from 'react';
-import { Text, StyleSheet } from 'react-native';
+import React from 'react';
+import { Button, View, Text, StyleSheet } from 'react-native';
 
+const HomeScreen = props => {
+    return (
+        <View>
+            <Text style={styles.text}>Welcome User!</Text>
+            <Text style={styles.padding}>How to use?</Text>
+            <View style={styles.style1}>
+            <Button onPress={() => props.navigation.navigate('Database')} color='green' title='Access Database' />
+            <Button onPress={() => props.navigation.navigate('Upload')} color='green' title='Upload DICOM'/></View>
+            
+                <Text style={styles.padding}>1) ...</Text>
+                <Text style={styles.padding}>2) ...</Text>
+                <Text style={styles.padding}>3) ...</Text>
+            
+        </View>);
+};
 
-export default class HomeScreen extends Component{
-    render(){
-        const styles = StyleSheet.create({
-            text: {
-                fontSize: 30
-            }
-        });
-        return <Text style={styles.text}>Hi there!</Text>;
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 30
+    },
+    padding: {
+        paddingTop: 20
+    },
+    style1: {
+        position: 'absolute',
+        right: 0,
+        bottom: 50
+    },
+    style2: {
+        margin: 20
     }
-}
+});
+
+export default HomeScreen;
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,52 +1,51 @@
-import React, { Component } from 'react';
-import { StyleSheet,Button } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, TextInput, StyleSheet, Button } from 'react-native';
+// import { Extypo } from '@expo/vector-icons';
 
 
-export default class LoginScreen extends Component{
-    constructor(props){
-        super(props)
-        this.username = ""
-        this.userpassword = "" 
-    }
-    setUserName = (e) => {
-        this.username = e
-    }
-    setPassword = (e) => {
-        this.userpassword = e
-    }
-    doNothing = () => {
-    
-    }
-    onSubmit = async() => {
+const LoginScreen = props => {
+    const [username, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onSubmit = async () => {
         const reqOption = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ username: this.username, password: this.userpassword })
+            body: JSON.stringify({ username: username, password: password })
         };
-        let resp = await fetch('/login', reqOption).then(data=>data.json())
-        return resp.success ? this.props.navigation.navigate('Home') : this.doNothing()
+        let resp = await fetch('/login', reqOption).then(data => data.json())
+        return resp.success ? props.navigation.navigate('Home') : doNothing()
     }
 
-    render(){
-        // const styles = StyleSheet.create({
-        //     text: {
-        //         fontSize: 30
-        //     },
-        // });
-        return (
-            <form  >
-                <label id="uname"><b>Username</b></label>
-                <input type="text" placeholder="Enter Username" name="username" onChange={(e) => this.setUserName(e.target.value)} required />
-                <label id="psw"><b>Password</b></label>
-                <input type="password" placeholder="Enter Password" name="userpassword" onChange={(e) => this.setPassword(e.target.value)} required />
-                <Button color='green' title='Login' onPress= {() => this.onSubmit() } id= "submit"/>
-                <label>
-                    <input type="checkbox" name="remember" onChange={this.doNothing} /> Remember me
-                    </label>
-            </form>
-        );
+    const checkInput = (usernameInput, passwordInput) => {
+        if (usernameInput == '' || passwordInput == '') return false;
+        return true;
     }
+
+    const doNothing = () => { }
+
+    return (
+        <View>
+            <Text>Username</Text>
+            <TextInput placeholder="Enter username" autoCapitalize="none" autoCorrect={false} value={username} onChangeText={(newValue) => setUserName(newValue)}></TextInput>
+            <Text>Password</Text>
+            <TextInput secureTextEntry={password} placeholder="Enter password" autoCapitalize="none" autoCorrect={false} value={password} onChangeText={(newValue) => setPassword(newValue)}></TextInput>
+            <Button color='green' title='Login' onPress={() => checkInput(username, password) ? onSubmit() : doNothing} />
+            <label>
+                <input type="checkbox" name="remember" onChange={doNothing} /> Remember me
+                    </label>
+        </View>
+    );
 };
+
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 30
+    },
+});
+
+export default LoginScreen;
+
 
 
 
