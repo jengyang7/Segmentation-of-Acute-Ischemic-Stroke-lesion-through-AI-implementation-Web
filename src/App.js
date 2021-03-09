236dbd4205daf55127576d8439978ceb6eb3512e
cheahshuen <kche0071@student.monash.edu>
2021-03-09 12:37:19 +0800

    Added database and upload screen.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import { createAppContainer} from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
-
+import DatabaseScreen from './screens/DatabaseScreen';
+import UploadScreen from './screens/UploadScreen';
+import logo from './logo.svg';
 
 const navigator =  createStackNavigator(
   {
     Main: LoginScreen,
-    Home: HomeScreen
+    Home: HomeScreen,
+    Database: DatabaseScreen,
+    Upload: UploadScreen
   },
   {
     initialRouteName: 'Main',
