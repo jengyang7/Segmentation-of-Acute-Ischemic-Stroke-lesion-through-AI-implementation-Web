77ca2dc9209a2e9fa66008a951a9170febbd8261
cheahshuen <kche0071@student.monash.edu>
2021-05-19 12:56:10 +0800

    Added new files

---
+++ src/screens/ResultScreen.js
@@ -0,0 +1,56 @@
+import React, { useContext, useEffect } from 'react';
+import { TouchableOpacity, Dimensions, Image, Text, View, TextInput, StyleSheet } from 'react-native';
+import { Context } from '../context/WebContext';
+import { navigate } from '../navigationRef';
+import FeatherIcon from 'feather-icons-react';
+import { globalStyle } from '../styles/global';
+
+
+const ResultScreen = () => {
+    return (
+        <View style={{ padding: height * 0.03 }}>
+            <Text style={globalStyle.titleText}>
+                Result
+            </Text>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginTop: 50 }}>
+
+                <View>
+                    <View style={{ flexDirection: 'row' }}>
+                        <FeatherIcon
+                            style={{ marginRight: width * 0.08, marginTop: height * 0.2 }}
+                            icon='chevron-left'
+                            onClick={() => { }}
+                        />
+                        <Image
+                            style={{ width: width * 0.3, height: height * 0.5 }}
+                            accessible={true}
+                            accessibilityLabel='A stroke image'
+                            accessibilityHint='Click to zoom in the image.'
+                            source={require('../images/Capture2.JPG')}
+                        />
+                        <FeatherIcon
+                            style={{ marginLeft: width * 0.08, marginTop: height * 0.2 }}
+                            icon='chevron-right'
+                            onClick={() => { }}
+                        />
+                    </View>
+                    <TouchableOpacity style={{ marginTop: 20, alignSelf: 'center', backgroundColor: 'black', alingItems: 'center', borderRadius: 4, padding: 8 }}>
+                        <Text style={globalStyle.buttonText}>
+                            Delete image
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={{ borderWidth: 1 }}>
+                    <Text style={globalStyle.subTitleText}>
+                        Diagnosis details
+                    </Text>
+                </View>
+            </View>
+        </View>
+    );
+};
+
+const { height, width } = Dimensions.get('window');
+
+export default ResultScreen;
---
+++ src/styles/global.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+export const globalStyle = StyleSheet.create({
+    titleText: {
+        fontFamily: '-apple-system',
+        fontSize: 50,
+        fontWeight: 'bold',
+        alignSelf: 'center'
+    },
+    subTitleText: {
+        fontFamily: '-apple-system',
+        fontSize: 25
+    },
+    infoText: {
+        fontFamily: '-apple-system',
+        fontSize: 20
+    },
+    buttonText: {
+        fontFamily: '-apple-system',
+        fontSize: 14,
+        color: 'white'
+    }
+});
